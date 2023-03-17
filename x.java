bde9ed14d07d15745a1c27783a40f08f924923d8
B153Prem <sirsikarp@gmail.com>
2023-03-17 20:03:11 +0530

    your x file are some code are write

---
+++ x.java
@@ -0,0 +1,24 @@
+public class X
+{
+
+    public void m1
+    {
+       System.out.println("m1");
+    }
+    public void m2
+    {
+       System.out.println("m2");
+    }
+    public void m3
+    {
+      System.out.println("m3");
+    }
+
+     public static void main
+     {
+        X x=new X();
+          x.m1();
+          x.m2();
+          x.m3();
+     }
+}
