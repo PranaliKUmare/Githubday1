0a9fce6fb692677c45f294e32fc3adc41e805ff6
PranaliKUmare <pranali.umare08@gmail.com>
2023-03-26 17:56:16 +0530

    Spring IOC-BASIC project

---
+++ ioc_practical1/src/com/model/Student.java
@@ -0,0 +1,17 @@
+package com.model;
+
+public class Student 
+{
+	
+	public Student () 
+	{
+		
+		System.out.println("This is student class constructor");
+	}
+	
+	public void display()
+	{
+		System.out.println("display");
+	}
+
+}
